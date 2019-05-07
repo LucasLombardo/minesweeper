269c551270b49005da8ea76e3bf0633dd67ef55d
LucasLombardo <lucasalombardo@gmail.com>
2019-05-06 23:50:04 -0400

    add baseline space exposing and flagging logic

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
 
 const Space = ({ spaceData, boardState }) => {
-    const [showSpace, setShowSpace] = useState(true)
+    const [showSpace, setShowSpace] = useState(false);
+    const [flagged, setFlagged] = useState(false);
 
     const handleClick = () => {
+        const { exposedSpaces, setExposedSpaces, status, setStatus } = boardState;
         setShowSpace(true);
+        setExposedSpaces(exposedSpaces + 1);
+    }
+
+    const handleLeftClick = e => {
+        e.preventDefault();
+        setFlagged(!flagged);
     }
 
     const display = spaceData.isMine ? '💣' : spaceData.value;
+    const unshownDisplay = flagged ? '🚩' : '';
     return (
-        <button onClick={handleClick}>
-            {showSpace ? display : ''}
+        <button onClick={handleClick} onContextMenu={handleLeftClick}>
+            {showSpace ? display : unshownDisplay}
         </button>
     )
 }
